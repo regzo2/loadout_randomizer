b3a70a877ecbb1a8814d6f625594e3331b150360
regzo2 <regzo252@gmail.com>
2026-01-02 20:15:18 -0500

    Added more test functions

--- scripts/loadout_randomizer_tests.lua
+++ scripts/loadout_randomizer_tests.lua
@@ -2,7 +2,7 @@ local mod = get_mod("loadout_randomizer")
 
 local LoadoutRandomizerGenerator = mod:io_dofile("loadout_randomizer/scripts/loadout_randomizer_generator")
 
-local generate_randomization_dataset = function()
+local generate_weapon_randomization_dataset = function()
 
     local datas = {}
 
@@ -39,4 +39,52 @@ local generate_randomization_dataset = function()
 
 end
 
-mod.generate_randomization_dataset = generate_randomization_dataset
+local generate_lonewolf_randomization_dataset = function()
+
+    local datas = {}
+    datas.blitz = {}
+    datas.keystones = {}
+    datas.aura = {}
+    datas.talent_conflicts = 0
+
+    gbl_d = datas
+
+    for i=0, 1000 do
+        local data = LoadoutRandomizerGenerator.generate_random_loadout(
+            {
+                "ability",
+                "aura",
+                "keystone",
+                "tactical", --blitz
+            },
+            "adamant"
+        )
+
+        local keystones = data.talents.keystone or nil
+        local blitz = data.talents.tactical or nil
+        local aura = data.talents.aura or nil
+
+        if keystones and blitz then
+            if keystones["adamant_disable_companion"] then
+                mod:echo("lonewolf!!")
+                if blitz["adamant_whistle"] then
+                    mod:echo("wtf lonewolf!!")
+                    datas.talent_conflicts = datas.talent_conflicts + 1
+                end
+                if aura["adamant_companion_coherency"] then
+                    mod:echo("wtf lonewolf!!")
+                    datas.talent_conflicts = datas.talent_conflicts + 1
+                end
+            end
+            for id, keystone in pairs(keystones) do
+                datas.keystones[id] = keystone
+            end
+            for id, keystone in pairs(aura) do
+                datas.aura[id] = keystone
+            end
+        end
+    end
+
+end
+
+mod.generate_randomization_dataset = generate_lonewolf_randomization_dataset
