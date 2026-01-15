e589ec49f590de5c8c1d7358745dd888691cc0d9
regzo2 <regzo252@gmail.com>
2026-01-14 19:10:41 -0500

    Removed talent class from profile.

--- scripts/loadout_randomizer_profile.lua
+++ scripts/loadout_randomizer_profile.lua
@@ -1,7 +1,6 @@
 local mod = get_mod("loadout_randomizer")
 
 local LoadoutRandomizerInventory    = mod:io_dofile("loadout_randomizer/scripts/loadout_randomizer_profile_inventory")
-local LoadoutRandomizerTalents      = mod:io_dofile("loadout_randomizer/scripts/loadout_randomizer_profile_talents")
 local LoadoutRandomizerProfileUtils = mod:io_dofile("loadout_randomizer/scripts/loadout_randomizer_profile_utils")
 
 LoadoutRandomizerProfile = {}
@@ -15,9 +14,7 @@ LoadoutRandomizerProfile.apply_randomizer_loadout_to_profile_preset = function(d
     local profile_preset = LoadoutRandomizerProfileUtils.get_randomizer_profile(profile)
 
     Managers.data_service.gear:fetch_inventory(character_id):next(function (inventory_items)
-        LoadoutRandomizerInventory.apply_inventory_loadout(data, profile_preset, character_id, inventory_items)
-        LoadoutRandomizerTalents.apply_talents_loadout(data, profile_preset, character_id)
-
+        LoadoutRandomizerInventory.apply_loadout(data, profile_preset, character_id, inventory_items)
         LoadoutRandomizerProfileUtils.save_randomizer_profile(profile_preset, character_id)
     end)
 end
